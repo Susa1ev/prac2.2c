2cb23cf4108e8033f2c8a8b069818679a0f56487
Susa1ev <susalytt@gmail.com>
2021-11-30 17:38:26 +0300

    Добавьте файлы проекта.

---
+++ prac2.2-c/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace prac2._2_c
+{
+    class prac
+    {
+        int ball;
+        string result;
+        public prac()
+        {
+            Console.WriteLine(countTheBall());
+        }
+        string countTheBall()
+        {
+            reTry:
+            Console.Write("Введите балл участника от 0 до 100 ->");
+            if(Int32.TryParse(Console.ReadLine(), out ball) && (ball >= 0) && (ball <= 100))
+            {
+                if(ball < 50)
+                {
+                    result = "неудовлетворительно";
+                }else if(ball < 70)
+                {
+                    result = "удовлетворительно";
+                }
+                else if (ball < 90)
+                {
+                    result = "хорошо";
+                }else if(ball < 100)
+                {
+                    result = "отлично";
+                }
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то");
+                goto reTry;
+            }            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
